bc31e3eb636e060473de759d3d4b23f9a97b99ef
Kalunia <pedro_grandin@hotmail.com>
2016-08-26 01:27:27 -0300

    fixing heroku port

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const milieu = require('milieu');
 const config = milieu('troca-facil-api', {
   environment: 'dev',
   server: {
-    port: 8080
+    port: process.env.PORT || 8080
   },
   mongo: {
     url: 'mongodb://heroku_1twx3vdm:j9gd660s1te0ir9to4fljnjcj9@ds017256.mlab.com:17256/heroku_1twx3vdm'
