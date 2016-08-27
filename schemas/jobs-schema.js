8c5b7b197a658790f40a0793ad84df848cca59dd
Pedro Henrique Grandin <pedro_grandin@hotmail.com>
2016-08-27 13:15:39 -0300

    Update jobs-schema.js

--- schemas/jobs-schema.js
+++ schemas/jobs-schema.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema, 
-	  ObjectId = Schema.ObjectId;
+const Schema   = mongoose.Schema;
 
 
 const JobsSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  user: { type: ObjectId, ref: 'UsersSchema' },
-  location: { type: ObjectId, ref: 'LocationsSchema' },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
+  location: { type: mongoose.Schema.Types.ObjectId, ref: 'Locations' },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date }
 });
